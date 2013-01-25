12cfe8ff0e65a1fbdf9f5cb199a8b87d42ec049c
hamogu <moritz.guenther@gmx.de>
2013-01-25 12:10:31 -0500

    make mock module for matplotlib.pylab so readthedocs works

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'pylab', 'pyfits']
+MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'pylab', 'pyfits', 'matplotlib.pylab']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
