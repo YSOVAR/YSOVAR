b908a73d34e143734ec912752906e8d4daaf76cf
hamogu <moritz.guenther@gmx.de>
2013-01-25 10:18:29 -0500

    add basic setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='pYSOVAR',
+      version='0.6',
+      py_modules=['lightcurves', 'ysovar_atlas','great_circle_dist','check_crossmatch_checkfilter', 'plot_atlas', 'ysovar_lombscargle'],
+      )
