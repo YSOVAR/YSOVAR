dd05b7108a30347463a2c57de67e528c7e280434
hamogu <moritz.guenther@gmx.de>
2013-07-03 14:23:13 -0400

    Change to IAU complient names

--- test/test_atlas.py
+++ test/test_atlas.py
@@ -16,10 +16,10 @@ def test_makecrossids():
     assert len(cid) == len(d1)
     
 def test_isoy2radec():
-    ra, dec = atlas.Isoy2radec('ISOY_J162722.02-242114.5')
+    ra, dec = atlas.IAU2radec('SSTYSV_J162722.02-242114.5')
     assert np.abs(ra - 246.84175) < 0.00001
     assert np.abs(dec + 24.354028) < 0.00001
-    ra, dec = atlas.Isoy2radec('ISOY_J162722.02+242114.5')
+    ra, dec = atlas.IAU2radec('SSTYSV_J162722.02+242114.5')
     assert np.abs(dec - 24.354028) < 0.00001
 
 def test_makecrossids_all():
